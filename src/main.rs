94a04993ea8dbe1dca9863b7ab07187a100850f8
Yash Shinde <shinde27yash@gmail.com>
2024-03-07 17:15:53 +0530

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,11 @@ async fn main() {
     let args = Args::parse();
     let query = args.query;
     match search(query).await {
-        Ok(search) => match search {
-            Search::Definition(definition) => println!("{}", definition),
-            Search::Suggestions(suggestions) => println!(
-                "The word you've entered isn't in the dictionary. Here are some suggestions:\n{}",
-                suggestions
-            ),
-        },
-        Err(error) => println!("{}", error),
+        Ok(Search::Definition(d)) => println!("{}", d),
+        Ok(Search::Suggestions(s)) => println!(
+            "The word you've entered isn't in the dictionary. Here are some suggestions:\n{}",
+            s
+        ),
+        Err(e) => println!("{}", e),
     }
 }
